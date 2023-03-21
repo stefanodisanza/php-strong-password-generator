f7c365b5d59860fbd5249d9a14873de3b02fa24f
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-03-21 15:07:20 +0100

    FEAT new button genera un'altra password

--- mostra_password.php
+++ mostra_password.php
@@ -4,3 +4,9 @@ session_start();
 $password = $_SESSION['password'];
 echo "La tua password casuale è: $password";
 ?>
+
+<br>
+<button>
+    <a href="index.php">Genera un'altra password</a>
+</button>
+
