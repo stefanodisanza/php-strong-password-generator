49468f85be93d7dd7d557ea0ba6ce64aaf9bc5a4
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-03-21 14:51:54 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    
+    <?php
 if (isset($_GET['lunghezza'])) {
     $lunghezza = $_GET['lunghezza'];
     $password = generaPassword($lunghezza);
@@ -23,3 +33,7 @@ function generaPassword($lunghezza = 10) {
   <br>
   <input type="submit" value="Genera Password">
 </form>
+
+</body>
+</html>
+
